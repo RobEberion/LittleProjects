98500bf902b516e3b22383470d181f6a55ace2ae
Eberion <robertolejnik@tuta.com>
2025-04-06 11:46:10 +0200

    Add files via upload

---
+++ Projects C#/Hangman/Hangman.cs
@@ -0,0 +1,270 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Projektsammlung.Spiele
+//{
+//    internal class Hangman
+//    {
+//        static void Main(string[] args)
+//        {
+//            MainMenu();
+
+//        }
+
+//        static void MainMenu()
+//        {
+//            while (true)
+//            {
+//                PrintHead();
+
+//                Console.WriteLine("Wähle eine Aktion aus:");
+//                Console.WriteLine();
+//                Console.ForegroundColor = ConsoleColor.Green;
+//                Console.WriteLine("[1] Spielen");
+//                Console.WriteLine("[2] Beenden");
+//                Console.ResetColor();
+//                Console.WriteLine();
+
+//                Console.Write("Aktion: ");
+
+//                int action = 0;
+
+//                do
+//                {
+//                    try
+//                    {
+//                        action = Convert.ToInt32(Console.ReadLine());
+//                        if (action != 1 && action != 2)
+//                        {
+
+//                            Console.ForegroundColor = ConsoleColor.Red;
+//                            Console.WriteLine("Nur 1 oder 2!");
+//                            Console.WriteLine();
+//                            Console.WriteLine("Versuche es erneut!");
+//                            Console.ResetColor();
+//                            Console.ReadKey();
+//                            break;
+//                        }
+//                    }
+//                    catch (Exception)
+//                    {
+
+//                        Console.ForegroundColor = ConsoleColor.Red;
+//                        Console.WriteLine("Du hast nur die Wahl zwischen 1 und 2.");
+//                        Console.WriteLine();
+//                        Console.WriteLine("Versuche es erneut!");
+//                        Console.ResetColor();
+//                        Console.ReadKey();
+//                        break;
+//                    }
+//                } while (action != 1 && action != 2);
+
+//                bool end = false;
+
+//                switch (action)
+//                {
+//                    case 1:
+//                        StartGame();
+//                        break;
+
+//                    case 2:
+//                        end = true;
+//                        break;
+//                }
+
+//                if (end)
+//                {
+//                    break;
+//                }
+
+//                Console.Clear();
+//            }
+
+//            static void StartGame()
+//            {
+//                string[] words = new string[]
+//                {
+//                    "Apfelkuchen",
+//                    "Gemüsesuppe",
+//                    "Kraftfahrzeug",
+//                    "Lastwagen",
+//                    "VideoSpiele",
+//                    "Reagenzglas",
+//                    "Briefmarke",
+//                    "Papierkorb",
+//                    "Aufenthalt",
+//                    "Abnehmen",
+//                    "Wanderung",
+//                    "Operationssaal",
+//                    "Telefonbuch",
+//                    "Kugelschreiber",
+//                    "Zusammenfassung",
+//                    "Ziegelsteine",
+//                    "Krebstier",
+//                    "Staubsauger",
+//                    "Sonnensegel",
+//                    "Konsonanten",
+//                    "Funktion",
+//                    "Gleichgewicht",
+//                    "Thermostat",
+//                    "Verbrennung",
+//                    "Viehzucht",
+//                    "Nachmittag",
+//                    "Traurigkeit",
+//                    "Gefrierfach",
+//                    "Scheinwerfer",
+//                    "Mittagsschlaf",
+//                    "Blitzableiter",
+//                    "Geschlossen",
+//                    "Zigarettenstummel",
+//                    "Backenzahn",
+//                    "Polizeistation",
+//                    "Bergsteiger",
+//                    "Lagerfeuer",
+//                    "Friseursalon",
+//                    "Himmelsrichtungen",
+//                    "Leichtathletik",
+//                    "Sammlung"
+//                };
+
+//                Random rnd = new Random();
+//                int index = rnd.Next(0, words.Length);
+//                string word = words[index].ToLower();
+//                GameLoop(word);
+
+//            }
+
+//            static void GameLoop(string word)
+//            {
+//                int lives = 10;
+//                string hiddenWord = "";
+
+//                for (int i = 0; i < word.Length; i++)
+//                {
+//                    hiddenWord += "_";
+//                }
+
+//                while (true)
+//                {
+
+//                    char character = '|';
+
+//                    while (character == '|')
+//                    {
+//                        Console.Clear();
+
+//                        PrintHead();
+
+//                        Console.WriteLine($"Gesuchtes Wort: {hiddenWord}");
+//                        Console.Write("Noch übrige Versuche: ");
+
+//                        for (int i = 0; i < lives; i++)
+//                        {
+//                            Console.ForegroundColor = ConsoleColor.Green;
+//                            Console.Write("+");
+//                            Console.ResetColor();
+//                        }
+
+//                        Console.WriteLine();
+
+//                        try
+//                        {
+//                            Console.Write("Buchstabe: ");
+//                            character = Convert.ToChar(Console.ReadLine().ToLower());
+//                        }
+//                        catch (FormatException)
+//                        {
+//                            Console.ForegroundColor = ConsoleColor.Red;
+//                            Console.WriteLine("Nur ein Buchstabe bitte!");
+//                            Console.ResetColor();
+//                            Console.ReadKey();
+//                        }
+//                    }
+
+
+
+
+//                    bool foundCharacterInWord = false;
+
+//                    for (int i = 0; i < word.Length; i++)
+//                    {
+//                        if (word[i] == character)
+//                        {
+//                            foundCharacterInWord = true;
+//                            break;
+//                        }
+//                    }
+
+//                    string tempHiddenWord = hiddenWord;
+//                    hiddenWord = "";
+
+//                    if (foundCharacterInWord)
+//                    {
+//                        for (int i = 0; i < word.Length; i++)
+//                        {
+//                            if (word[i] == character)
+//                            {
+//                                hiddenWord += character;
+//                            }
+//                            else if (tempHiddenWord[i] != '_')
+//                            {
+//                                hiddenWord += tempHiddenWord[i];
+//                            }
+//                            else
+//                            {
+//                                hiddenWord += '_';
+//                            }
+//                        }
+
+//                        if (hiddenWord == word)
+//                        {
+//                            Console.Clear();
+//                            PrintHead();
+//                            Console.ForegroundColor = ConsoleColor.Green;
+//                            Console.WriteLine("Gewonnen!!!");
+//                            Console.WriteLine($"Das Wort war: {word}");
+//                            Console.ReadKey();
+//                            Console.ResetColor();
+//                            break;
+//                        }
+
+//                    }
+//                    else
+//                    {
+//                        hiddenWord = tempHiddenWord;
+
+//                        if (lives > 1)
+//                        {
+//                            lives -= 1;
+//                        }
+//                        else
+//                        {
+//                            Console.Clear();
+//                            PrintHead();
+//                            Console.ForegroundColor = ConsoleColor.Red;
+//                            Console.WriteLine("GAME OVER!!!");
+//                            Console.ReadKey();
+//                            Console.ResetColor();
+//                            break;
+//                        }
+
+//                    }
+//                }
+//            }
+//            static void PrintHead()
+//            {
+//                Console.WriteLine("|---------------------------|");
+//                Console.Write("| O(^-^)=O ");
+//                Console.ForegroundColor = ConsoleColor.Red;
+//                Console.Write("HANGMAN");
+//                Console.ResetColor();
+//                Console.WriteLine(" O=(-.-)O |");
+//                Console.WriteLine("|---------------------------|");
+//                Console.WriteLine();
+//            }
+//        }
+//    }
+//}
---
+++ Projects C#/ZahlenRaten/Zahlenratespiel.cs
@@ -0,0 +1,86 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Projektsammlung.Spiele
+//{
+//    internal class Zahlenratespiel
+//    {
+//        static void Main(string[] args)
+//        {
+//            int MinValue = 0;
+//            int MaxValue = 1000;
+//            int Again = 1;
+
+//            Random rnd = new Random();
+//            int RandomNumber = rnd.Next(MinValue, MaxValue);
+//            //Console.WriteLine(RandomNumber);
+
+//            Console.WriteLine($"Hallo, ich merke mir eine Zahl zwischen {MinValue} und {MaxValue}.");
+//            Console.WriteLine("Was ist meine Zahl?");
+//            int Number = Convert.ToInt32(Console.ReadLine());
+
+//            while (Number != RandomNumber)
+//            {
+//                Again++;
+
+//                if ((Number < RandomNumber) && ((RandomNumber - Number) >= 300))
+//                {
+//                    Console.WriteLine("Die Zahl ist sehr viel größer!");
+//                    Number = Convert.ToInt32(Console.ReadLine());
+//                }
+//                else if ((Number < RandomNumber) && ((RandomNumber - Number) >= 150))
+//                {
+//                    Console.WriteLine("Die Zahl ist viel größer!");
+//                    Number = Convert.ToInt32(Console.ReadLine());
+//                }
+//                else if ((Number < RandomNumber) && ((RandomNumber - Number) >= 50))
+//                {
+//                    Console.WriteLine("Die Zahl ist größer!");
+//                    Number = Convert.ToInt32(Console.ReadLine());
+//                }
+//                else if ((Number < RandomNumber) && ((RandomNumber - Number) >= 10))
+//                {
+//                    Console.WriteLine("Die Zahl ist ein bisschen größer!");
+//                    Number = Convert.ToInt32(Console.ReadLine());
+//                }
+//                else if ((Number < RandomNumber) && ((RandomNumber - Number) < 10))
+//                {
+//                    Console.WriteLine("Die Zahl ist ein kleines bisschen größer!");
+//                    Number = Convert.ToInt32(Console.ReadLine());
+//                }
+//                else if ((Number > RandomNumber) && ((Number - RandomNumber) >= 300))
+//                {
+//                    Console.WriteLine("Die Zahl ist sehr viel kleiner!");
+//                    Number = Convert.ToInt32(Console.ReadLine());
+//                }
+//                else if ((Number > RandomNumber) && ((Number - RandomNumber) >= 150))
+//                {
+//                    Console.WriteLine("Die Zahl ist viel kleiner!");
+//                    Number = Convert.ToInt32(Console.ReadLine());
+//                }
+//                else if ((Number > RandomNumber) && ((Number - RandomNumber) >= 50))
+//                {
+//                    Console.WriteLine("Die Zahl ist kleiner!");
+//                    Number = Convert.ToInt32(Console.ReadLine());
+//                }
+//                else if ((Number > RandomNumber) && ((Number - RandomNumber) >= 10))
+//                {
+//                    Console.WriteLine("Die Zahl ist ein bisschen kleiner!");
+//                    Number = Convert.ToInt32(Console.ReadLine());
+//                }
+//                else if ((Number > RandomNumber) && ((Number - RandomNumber) < 10))
+//                {
+//                    Console.WriteLine("Die Zahl ist ein kleines bisschen kleiner!");
+//                    Number = Convert.ToInt32(Console.ReadLine());
+//                }
+
+//            }
+
+//            Console.WriteLine($"Herzlichen Glückwunsch!!! Die Zahl {RandomNumber} ist richtig, du hast {Again} Versuche gebraucht. :) ");
+//            Console.ReadKey();
+//        }
+//    }
+//}
